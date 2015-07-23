50104f21ea4c144c180fbb2afe2ac67451875624
James Manton <ajd.manton@googlemail.com>
2015-07-23 14:35:28 +0100

    Add sigma deque

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 deque<float> x_data, y_data, z_data;
+deque<float> sigma;
 
 // Calculates the mean of a deque
 float average(deque<float> x) {
@@ -40,12 +41,16 @@ void update_data() {
 		printf("%f, %f, %f\n", x, y, z);
 		x_data.push_front(x); y_data.push_front(x); z_data.push_front(x);
 		x_data.pop_back(); y_data.pop_back(); z_data.pop_back();
+
+		sigma.push_front(sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)));
+		sigma.pop_back();
 	}
 }
 
 int main() {
 	srand(time(NULL));
 	x_data.assign(20, 0); y_data.assign(20, 0); z_data.assign(20, 0);
+	sigma.assign(20, 0);
 
 	update_data();
 }
